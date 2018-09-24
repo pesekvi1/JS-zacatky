64a16e0ce3e60c9b8aec5c43b9b512aa8b257075
Viktor Pešek <viktor.pesek12@gmail.com>
2018-09-24 08:53:22 +0200

    deleting all files, now initializing ReactJS

--- src/js/ahoj.js
+++
@@ -1,4 +0,0 @@
-function ahoj(jmeno) {
-    alert("Ahoj jak se mas");
-}
-// this si javascript code
--- src/ts/ahoj.ts
+++
@@ -1,5 +0,0 @@
-function vypisSe(jmeno: string){
-    alert(jmeno);
-}
-
-// this is typescript code
